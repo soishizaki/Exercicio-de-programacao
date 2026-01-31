c5f246306a1bb2ce13b2a11c7293fd6d55409b6b
soishizaki <sofiasi1@al.insper.edu.br>
2026-01-31 13:40:32 -0300

    mais uma tentativa do programa

--- programa.py
+++ programa.py
@@ -1,70 +1,127 @@
 from funcoes import *
+import random
 
-n = int(input("Quantos jogadores? (2-4) "))
-while n < 2 and n > 4:
-    n = int(input("Quantos jogadores? (2-4) "))
+
+cores = {
+   0: "\033[37m",
+   1: "\033[34m",
+   2: "\033[33m",
+   3: "\033[32m",
+   4: "\033[35m",
+   5: "\033[31m",
+   6: "\033[36m",
+}
+reset = "\033[0m"
+
+
+numero = int(input("Quantos jogadores? (2-4) "))
+while numero < 2 and numero > 4:
+   numero = int(input("Quantos jogadores? (2-4) "))
+
 
 pecas = cria_pecas()
-jogo = inicia_jogo(n, pecas)
+jogo = inicia_jogo(numero, pecas)
 
-jogador_atual = random.randint(0, n - 1)
 
+jogador_atual = random.randint(0, numero - 1)
 sem_jogar_seguidos = 0
 
+
+print("\nMESA:\n")
+
+
 while True:
-    ganhador = verifica_ganhador(jogo["jogadores"])
-    if ganhador != -1:
-        print("MESA:", jogo["mesa"])
-        print("Vencedor:", ganhador + 1)
-        break
+   ganhador = verifica_ganhador(jogo["jogadores"])
+   if ganhador != -1:
+       print("MESA:", jogo["mesa"])
+       print("Vencedor:", ganhador + 1)
 
-    mao = jogo["jogadores"][jogador_atual]
-    mesa = jogo["mesa"]
+       vencedores = [ganhador]
+       menor = None
+       for j in jogo["jogadores"]:
+           pts = conta_pontos(jogo["jogadores"][0])
+           if menor is None or pts <= menor:
+               menor = pts
+       print("Menor pontuação:", menor)
+       break
 
-    print("\nMESA:", mesa)
 
-    poss = posicoes_possiveis(mesa, mao)
-    while len(poss) == 0 and len(jogo["monte"]) > 0:
-        mao.append(jogo["monte"][0])
-        jogo["monte"] = jogo["monte"][1:]
+   mao = jogo["jogadores"][jogador_atual]
+   mesa = jogo["mesa"]
 
-    if len(poss) == 0:
-        if jogador_atual == 0:
-            print("Você não tem jogadas e o monte acabou. Você passou.")
-        sem_jogar_seguidos += 1
-    else:
-        sem_jogar_seguidos += 1
 
-        if jogador_atual == 0:
-            print("Jogador: Você com", len(mao), "peça(s)")
-            for i in range(len(mao)):
-                print(str(i + 1) + ":", mao[i])
-            print("Posições possíveis:", [x + 1 for x in poss])
+   posicao = posicoes_possiveis(mesa, mao)
 
-            escolha = int(input("Escolha a peça: "))
-            while (escolha - 1) not in poss:
-                escolha = int(input("Escolha a peça: "))
-            idx = escolha
-        else:
-            print("Jogador:", jogador_atual, "com", len(mao), "peça(s)")
-            idx = random.choice(poss)
 
-        peca = mao.pop(idx)
-        jogo["mesa"] == adiciona_na_mesa(peca, jogo["mesa"])
-        print("Colocou:", peca)
+   if len(posicao) == 0:
+       if jogador_atual == 0:
+           while len(posicao) == 0 and len(jogo["monte"]) > 0:
+               print("Não tem peças possíveis. PEGANDO DO MONTE!")
+               input("[pressione ENTER]")
+               mao.append(jogo["monte"][0])
+               jogo["monte"] = jogo["monte"][1:]
+       else:
+           while len(posicao) == 0 and len(jogo["monte"]) > 0:
+               mao.append(jogo["monte"][0])
+               jogo["monte"] = jogo["monte"][1:]
 
-    if sem_jogar_seguidos >= n and len(jogo["monte"]) > 0:
-        print("\nJogo travado! (empate)")
-        menor = None
-        vencedores = []
-        for j in jogo["jogadores"]:
-            pontos = conta_pontos(jogo["jogadores"][0])
-            if menor is None or pontos <= menor:
-                menor = pontos
-                vencedores.append(j)
-        print("Menor pontuação:", menor)
-        print("Vencedor(es):", vencedores)
-        print("MESA:", jogo["mesa"])
-        break
 
-    jogador_atual = (jogador_atual + 1) % n
+   if len(posicao) == 0:
+       sem_jogar_seguidos = sem_jogar_seguidos + 1
+   else:
+       sem_jogar_seguidos = sem_jogar_seguidos + 1
+
+       if jogador_atual == 0:
+           print("Jogador: Você com", len(mao), "peça(s)\n")
+
+           for i in range(len(mao)):
+               print(str(i + 1) + ":", mao[i])
+
+           escolha = int(input("\nEscolha a peça: "))
+           indice_escolhido = escolha
+
+           if indice_escolhido <= 0 or indice_escolhido > len(mao):
+               print("Não é possível jogar essa peça")
+           elif indice_escolhido - 1 not in posicao:
+               print("Não é possível jogar essa peça")
+           else:
+               peca = mao.pop(indice_escolhido)
+               jogo["mesa"] = adiciona_na_mesa(peca, jogo["mesa"])
+               print("Colocou:", peca, "\n")
+
+       else:
+           print("Jogador:", jogador_atual + 1, "com", len(mao), "peça(s)")
+           if len(posicao) > 0:
+               indice = random.randint(0, len(mao) - 1)
+           else:
+               indice = 0
+           if indice in posicao:
+               peca = mao.pop(indice)
+           else:
+               peca = mao.pop(0)
+           jogo["mesa"] = adiciona_na_mesa(peca, jogo["mesa"])
+           print("Colocou:", peca, "\n")
+
+
+   print("MESA:", jogo["mesa"])
+   print()
+
+
+   if sem_jogar_seguidos >= numero and len(jogo["monte"]) > 0:
+       print("Empate! \n")
+
+       menor = None
+       vencedores = []
+
+       for j in jogo["jogadores"]:
+           pts = conta_pontos(jogo["jogadores"][j])
+           if menor is None or pts <= menor:
+               menor = pts
+               vencedores.append(j)
+
+       print("Menor pontuação:", menor)
+       print("VENCEDOR(ES):", vencedores)
+       break
+
+
+   jogador_atual = (jogador_atual + 1) % (numero - 1)
